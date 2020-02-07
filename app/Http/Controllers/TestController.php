39292fbe3b8f2790700adff10966babba14f6f83
xin <xin@localhost.localdomain>
2020-02-07 07:12:49 +0000

    pub sign

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,11 +12,12 @@ class TestController extends Controller
         $sign = $_GET['sign'];//传过来的签名
         $key = "1905";
 
-        $check = md5($data.$key);
+        $check = md5($data . $key);
 
-        if($check != $sign){
-            echo '验签失败';die;
-        }else{
+        if ($check != $sign) {
+            echo '验签失败';
+            die;
+        } else {
             echo '验签成功';
         }
     }
@@ -29,13 +30,36 @@ class TestController extends Controller
         $sign = $_POST['sign'];
         echo '<br>';
         //计算签名
-        $check = md5($data.$key);
-        echo '接受签名：'.$check;
+        $check = md5($data . $key);
+        echo '接受签名：' . $check;
         //对比接收过来的签名
-        if($check != $sign){
+        if ($check != $sign) {
             echo '验签失败';
-        }else{
+        } else {
             echo '验签成功';
+        }
+    }
+
+    public function key_sign()
+    {
+        $sign = $_GET['sign'];
+//        $sign = 'asdsadsadasdsad';
+        //字典排序
+        $data = $_GET['data'];
+        print_r($_GET);
+
+
+        //使用公钥验签
+        $pub_key = storage_path('keys/pub.key');
+        $pkeyid = openssl_pkey_get_public("file://" . $pub_key);
+        $status = openssl_verify($data, base64_decode($sign), $pkeyid);
+        var_dump($status);
+        echo '<br>';
+
+        if ($status) {
+            echo '验签成功';
+        } else {
+            echo '验签失败';
         }
     }
 }
